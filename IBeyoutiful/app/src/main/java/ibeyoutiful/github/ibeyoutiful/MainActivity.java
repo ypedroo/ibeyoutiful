78ebe2ecb99dd0081c996395e425388c8cb303a3
ynoa pedro <ynoa.pedro@outlook.com>
2019-03-04 03:41:39 -0300

    Adding the new project file

---
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/MainActivity.java
@@ -0,0 +1,13 @@
+package ibeyoutiful.github.ibeyoutiful;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
