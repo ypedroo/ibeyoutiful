497eb9d0177b6e8d455b05d0ab510a95c22c7233
ynoa pedro <ynoa.pedro@outlook.com>
2019-03-10 16:31:50 -0300

    Adding login configurations

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/AutenticacaoActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/AutenticacaoActivity.java
@@ -2,15 +2,33 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import ibeyoutiful.github.ibeyoutiful.R;
 
 public class AutenticacaoActivity extends AppCompatActivity {
+
+    private Button botaoAcessar;
+    private EditText campoEmail,campoSenha;
+    private Switch tipoAcesso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autenticacao);
         getSupportActionBar().hide();
+
+        inicialiazarComponentes();
+
+        
+    }
+
+    private void inicialiazarComponentes(){
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        botaoAcessar = findViewById(R.id.buttonAcesso);
+        tipoAcesso = findViewById(R.id.switchAcesso);
     }
 }
