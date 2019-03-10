af90c693bfcb0341bc5fd6743caee3b1d2e05db5
ynoa pedro <ynoa.pedro@outlook.com>
2019-03-10 12:21:54 -0300

    Adding new folder structure

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/AutenticacaoActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/AutenticacaoActivity.java
@@ -1,13 +1,13 @@
-package ibeyoutiful.github.ibeyoutiful;
+package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AutenticacaoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_autenticacao);
     }
 }
--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/SplashActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/SplashActivity.java
@@ -1,7 +1,9 @@
-package ibeyoutiful.github.ibeyoutiful;
+package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ibeyoutiful.github.ibeyoutiful.R;
 
 public class SplashActivity extends AppCompatActivity {
 
