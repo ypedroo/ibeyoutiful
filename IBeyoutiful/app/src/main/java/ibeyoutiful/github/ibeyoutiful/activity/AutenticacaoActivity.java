a838ec6ee29f1ff7936c66c5cbb1f19051078cdb
ypedro <ynoa.pedro@outlook.com>
2019-03-29 09:07:21 -0300

    Adding register function

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/AutenticacaoActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/AutenticacaoActivity.java
@@ -1,18 +1,32 @@
 package ibeyoutiful.github.ibeyoutiful.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import ibeyoutiful.github.ibeyoutiful.R;
+import ibeyoutiful.github.ibeyoutiful.helper.ConfiguracaoFirebase;
 
 public class AutenticacaoActivity extends AppCompatActivity {
 
     private Button botaoAcessar;
     private EditText campoEmail,campoSenha;
     private Switch tipoAcesso;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +35,60 @@ public class AutenticacaoActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         inicialiazarComponentes();
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
+        botaoAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
 
+                if (!email.isEmpty()){
+                    if (!senha.isEmpty()){
+                        //Verifica estado do switch entre cadastro e login.
+                        if(tipoAcesso.isChecked()){ //Cadastro
+
+                            autenticacao.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(AutenticacaoActivity.this,
+                                                "Usuário cadastrado com sucesso",
+                                                Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+
+                                    } else {
+                                        String erroExecao="";
+
+                                        try{
+                                            throw task.getException() ;
+                                        }catch(FirebaseAuthWeakPasswordException e){
+                                            erroExecao = "Digite uma senha mais forte";
+                                        }catch(FirebaseAuthInvalidCredentialsException e){
+                                            erroExecao = "Por favor digite um e-mail válido";
+                                        }catch (FirebaseAuthUserCollisionException e) {
+                                            erroExecao= "Essa conta ja foi cadastrada";
+                                        }catch (Exception e) {
+                                            erroExecao = "Ao cadastrar usuário: " + e.getMessage();
+                                            e.printStackTrace();
+                                        }
+
+                                        Toast.makeText(AutenticacaoActivity.this,
+                                                "Erro: " + erroExecao
+                                                , Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }else{ //Login
+                        }
+                    }else{
+                        Toast.makeText(AutenticacaoActivity.this, "Preencha a Senha", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(AutenticacaoActivity.this, "Preencha o Email", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void inicialiazarComponentes(){
