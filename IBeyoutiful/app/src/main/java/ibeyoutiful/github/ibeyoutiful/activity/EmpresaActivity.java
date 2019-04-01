0f90e1daa837253a0155bd470b99133108274a22
ypedro <ynoa.pedro@outlook.com>
2019-04-01 18:31:22 -0300

    Adding configuracoes activiy

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/EmpresaActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/EmpresaActivity.java
@@ -6,17 +6,22 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import ibeyoutiful.github.ibeyoutiful.R;
+import ibeyoutiful.github.ibeyoutiful.helper.ConfiguracaoFirebase;
 
 public class EmpresaActivity extends AppCompatActivity {
-
+    private FirebaseAuth autenticacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_empresa);
 
+        autenticacao= ConfiguracaoFirebase.getFirebaseAutenticacao();
         //Configuração Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("IBe you tiful - Profissional");
@@ -30,5 +35,31 @@ public class EmpresaActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_empresa, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                deslogarUsuario();
+                break;
+            case R.id.menuConfiguracoes:
+                abrirConfiguracoes();
+                break;
+            case R.id.menuNovoServico:
+                abrirNovoServico();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
