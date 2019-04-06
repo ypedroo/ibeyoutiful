d271b56989d7e06ceabd268fdf25d79c0f89b8e6
ypedro <ynoa.pedro@outlook.com>
2019-04-06 11:56:09 -0300

    Fixing sair option on menu

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/EmpresaActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/EmpresaActivity.java
@@ -63,6 +63,7 @@ public class EmpresaActivity extends AppCompatActivity {
     private void deslogarUsuario(){
         try{
             autenticacao.signOut();
+            finish();
         }catch (Exception e) {
             e.printStackTrace();
         }
