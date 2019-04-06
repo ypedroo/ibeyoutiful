427ecb8dfd9001095283f2e53d6c8f618b920aa4
ypedro <ynoa.pedro@outlook.com>
2019-04-06 11:26:22 -0300

    Adding the parents class on manifest

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/NovoServicoEmpresaActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/NovoServicoEmpresaActivity.java
@@ -2,6 +2,7 @@ package ibeyoutiful.github.ibeyoutiful;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class NovoServicoEmpresaActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class NovoServicoEmpresaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_servico_empresa2);
+
+        //Configuração Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Novo Serviço");
+        setSupportActionBar(toolbar);
+
     }
 }
