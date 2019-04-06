ff569bc804c446ecea8fe8ce7096ea8d1a1ef61c
ypedro <ynoa.pedro@outlook.com>
2019-04-06 12:40:24 -0300

    FInishing interfaces

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/ConfiguracoesUsuarioActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/ConfiguracoesUsuarioActivity.java
@@ -2,6 +2,7 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import ibeyoutiful.github.ibeyoutiful.R;
 
@@ -11,5 +12,11 @@ public class ConfiguracoesUsuarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes_usuario);
+
+        //Configuração Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Configurações Usuário");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
