49af0557de2080b394f1114b852a7fef05f4515a
ypedro <ynoa.pedro@outlook.com>
2019-04-20 12:18:07 -0300

    Adding methods to config the company

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/ConfiguracoesEmpresaActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/ConfiguracoesEmpresaActivity.java
@@ -3,20 +3,33 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import ibeyoutiful.github.ibeyoutiful.R;
 
 public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
 
+    private EditText editNomeEmpresa, editTipoServico, editValorMedio;
+    private ImageView perfilEmpresa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes_empresa);
+        incializarComponentes();
 
         //Configuração Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void incializarComponentes(){
+        editNomeEmpresa = findViewById(R.id.editNomeEmpresa);
+        editTipoServico = findViewById(R.id.editTipoServico);
+        editValorMedio = findViewById(R.id.editValorMedio);
+        perfilEmpresa = findViewById(R.id.perfilEmpresa);
     }
 }
