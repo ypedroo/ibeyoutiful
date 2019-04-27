7eb9aa63e064508b5a47ccfe2dc9dec55f61c717
ypedro <ynoa.pedro@outlook.com>
2019-04-27 15:34:59 -0300

    Creating produto model

---
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/model/Produto.java
@@ -0,0 +1,10 @@
+package ibeyoutiful.github.ibeyoutiful.model;
+
+public class Produto {
+
+    private String idUsuario;
+    private String nome;
+    private String descricao;
+    private String preco;
+
+}
