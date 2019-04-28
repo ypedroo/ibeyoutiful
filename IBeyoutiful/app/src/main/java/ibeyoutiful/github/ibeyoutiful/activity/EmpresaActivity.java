13bff5d2e8e8d906b327a19a94f1cb3395822346
ypedro <ynoa.pedro@outlook.com>
2019-04-28 18:43:58 -0300

    Addint product recycler view

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/EmpresaActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/EmpresaActivity.java
@@ -3,32 +3,90 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ibeyoutiful.github.ibeyoutiful.R;
+import ibeyoutiful.github.ibeyoutiful.adapter.AdapterProduto;
 import ibeyoutiful.github.ibeyoutiful.helper.ConfiguracaoFirebase;
+import ibeyoutiful.github.ibeyoutiful.helper.UsuarioFirebase;
+import ibeyoutiful.github.ibeyoutiful.model.Produto;
 
 public class EmpresaActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
+    private RecyclerView recyclerProdutos;
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtos = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idUsuarioLogado;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
 
+        inicializarComponentes();
         autenticacao= ConfiguracaoFirebase.getFirebaseAutenticacao();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         //Configuração Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("I Be you tiful - Profissional");
         setSupportActionBar(toolbar);
+
+        //Configuraçao recyclerView
+        recyclerProdutos.setLayoutManager( new LinearLayoutManager(this));
+        recyclerProdutos.setHasFixedSize(true);
+        adapterProduto = new AdapterProduto( produtos, this);
+        recyclerProdutos.setAdapter( adapterProduto );
+
+        //Recuperar Produtos para empresa
+        recuperarProdutos();
+
+    }
+
+    private void recuperarProdutos() {
+
+        final DatabaseReference produtosRef = firebaseRef
+                .child("produtos")
+                .child( idUsuarioLogado );
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                produtos.clear();
+
+                for ( DataSnapshot ds: dataSnapshot.getChildren()) {
+                    produtos.add( ds.getValue(Produto.class));
+                }
+
+                adapterProduto.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+     private void inicializarComponentes(){
+        recyclerProdutos = findViewById(R.id.recyclerProdutos);
 
     }
 
