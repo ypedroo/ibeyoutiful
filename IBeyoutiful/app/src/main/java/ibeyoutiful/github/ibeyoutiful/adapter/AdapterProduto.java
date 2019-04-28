681e5a51f6c71bb5cf307db8d6ddf269658226e2
ypedro <ynoa.pedro@outlook.com>
2019-04-28 15:13:42 -0300

    Adding Adapter Class

---
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/adapter/AdapterProduto.java
@@ -0,0 +1,64 @@
+package br.com.ifood.cursoandroid.ifoodprojeto.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.ifood.cursoandroid.ifoodprojeto.R;
+import br.com.ifood.cursoandroid.ifoodprojeto.model.Produto;
+
+/**
+ * Created by Jamilton
+ */
+
+public class AdapterProduto extends RecyclerView.Adapter<AdapterProduto.MyViewHolder>{
+
+    private List<Produto> produtos;
+    private Context context;
+
+    public AdapterProduto(List<Produto> produtos, Context context) {
+        this.produtos = produtos;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_produto, parent, false);
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
+        Produto produto = produtos.get(i);
+        holder.nome.setText(produto.getNome());
+        holder.descricao.setText(produto.getDescricao());
+        holder.valor.setText("R$ " + produto.getPreco());
+    }
+
+    @Override
+    public int getItemCount() {
+        return produtos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nome;
+        TextView descricao;
+        TextView valor;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            nome = itemView.findViewById(R.id.textNomeRefeicao);
+            descricao = itemView.findViewById(R.id.textDescricaoRefeicao);
+            valor = itemView.findViewById(R.id.textPreco);
+        }
+    }
+}
