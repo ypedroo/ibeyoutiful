cdda4458dadf2a817034a7e628dd3309406883f0
ypedro <ynoa.pedro@outlook.com>
2019-05-05 20:22:08 -0300

    Recovering companys data

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/CardapioActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/CardapioActivity.java
@@ -2,11 +2,23 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import ibeyoutiful.github.ibeyoutiful.R;
+import ibeyoutiful.github.ibeyoutiful.model.Empresa;
 
 public class CardapioActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerProdutosCardapio;
+    private ImageView imageEmpresaCardapio;
+    private TextView textNomeEmpresaCardapio;
+    private Empresa empresaSelecionada;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,17 @@ public class CardapioActivity extends AppCompatActivity {
         //Configuções Iniciais
         inicializarComponentes();
 
+        //Recuperar Emprsa
+        Bundle bundle = getIntent().getExtras();
+        if( bundle != null){
+            empresaSelecionada = (Empresa) bundle.getSerializable("empresa");
+
+            textNomeEmpresaCardapio.setText( empresaSelecionada.getNome() );
+
+            String url = empresaSelecionada.getUrlImagem();
+            Picasso.get().load(url).into( imageEmpresaCardapio );
+        }
+
         //Configuração Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Catálogo de Serviços");
@@ -24,6 +47,9 @@ public class CardapioActivity extends AppCompatActivity {
     }
 
     private void inicializarComponentes() {
+        recyclerProdutosCardapio = findViewById(R.id.recyclerProdutoCardapio);
+        imageEmpresaCardapio = findViewById(R.id.imageEmpresaCardapio);
+        textNomeEmpresaCardapio = findViewById(R.id.textNomeEmpresaCardapio);
 
     }
 }
