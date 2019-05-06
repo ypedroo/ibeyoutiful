95e5cb6e5dccccb0c8d291d1cebd2e183e54d88c
ypedro <ynoa.pedro@outlook.com>
2019-05-05 22:04:06 -0300

    Finishing User model

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/model/Usuario.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/model/Usuario.java
@@ -1,4 +1,48 @@
 package ibeyoutiful.github.ibeyoutiful.model;
 
 public class Usuario {
+
+    private String idUsuario;
+    private String urlImagem;
+    private String nomeUsuario;
+    private String enderecoUsuario;
+
+    public Usuario(String idUsuario, String urlImagem, String nomeUsuario, String enderecoUsuario) {
+        this.idUsuario = idUsuario;
+        this.urlImagem = urlImagem;
+        this.nomeUsuario = nomeUsuario;
+        this.enderecoUsuario = enderecoUsuario;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEnderecoUsuario() {
+        return enderecoUsuario;
+    }
+
+    public void setEnderecoUsuario(String enderecoUsuario) {
+        this.enderecoUsuario = enderecoUsuario;
+    }
 }
