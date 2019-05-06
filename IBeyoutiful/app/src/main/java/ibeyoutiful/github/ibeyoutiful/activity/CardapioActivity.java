6455a70eb1ee4e74c35e565912760a06ceb49c92
ypedro <ynoa.pedro@outlook.com>
2019-05-05 21:13:48 -0300

    Recovering catalog products

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/CardapioActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/CardapioActivity.java
@@ -2,15 +2,26 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ibeyoutiful.github.ibeyoutiful.R;
+import ibeyoutiful.github.ibeyoutiful.adapter.AdapterProduto;
+import ibeyoutiful.github.ibeyoutiful.helper.ConfiguracaoFirebase;
 import ibeyoutiful.github.ibeyoutiful.model.Empresa;
+import ibeyoutiful.github.ibeyoutiful.model.Produto;
 
 public class CardapioActivity extends AppCompatActivity {
 
@@ -18,6 +29,10 @@ public class CardapioActivity extends AppCompatActivity {
     private ImageView imageEmpresaCardapio;
     private TextView textNomeEmpresaCardapio;
     private Empresa empresaSelecionada;
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtos = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idEmpresa;
 
 
     @Override
@@ -27,6 +42,9 @@ public class CardapioActivity extends AppCompatActivity {
 
         //Configuções Iniciais
         inicializarComponentes();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+
+
 
         //Recuperar Emprsa
         Bundle bundle = getIntent().getExtras();
@@ -34,6 +52,7 @@ public class CardapioActivity extends AppCompatActivity {
             empresaSelecionada = (Empresa) bundle.getSerializable("empresa");
 
             textNomeEmpresaCardapio.setText( empresaSelecionada.getNome() );
+            idEmpresa = empresaSelecionada.getIdUsuario();
 
             String url = empresaSelecionada.getUrlImagem();
             Picasso.get().load(url).into( imageEmpresaCardapio );
@@ -44,6 +63,39 @@ public class CardapioActivity extends AppCompatActivity {
         toolbar.setTitle("Catálogo de Serviços");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Configuraçao recyclerView
+        recyclerProdutosCardapio.setLayoutManager( new LinearLayoutManager(this));
+        recyclerProdutosCardapio.setHasFixedSize(true);
+        adapterProduto = new AdapterProduto( produtos, this);
+        recyclerProdutosCardapio.setAdapter( adapterProduto );
+
+        //Recuperar Produtos para empresa
+        recuperarProdutos();
+    }
+    private void recuperarProdutos() {
+
+        final DatabaseReference produtosRef = firebaseRef
+                .child("produtos")
+                .child( idEmpresa );
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                produtos.clear();
+
+                for ( DataSnapshot ds: dataSnapshot.getChildren()) {
+                    produtos.add( ds.getValue(Produto.class));
+                }
+
+                adapterProduto.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void inicializarComponentes() {
