8dd08e981ac4c62a54f0b7a32a2ed6978689a5ab
ypedro <ynoa.pedro@outlook.com>
2019-05-17 00:18:06 -0300

    Adding pedidosAdapter class based on project course

--- IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/PedidosActivity.java
+++ IBeyoutiful/app/src/main/java/ibeyoutiful/github/ibeyoutiful/activity/PedidosActivity.java
@@ -2,14 +2,31 @@ package ibeyoutiful.github.ibeyoutiful.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import ibeyoutiful.github.ibeyoutiful.R;
 
 public class PedidosActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerPedidos;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos);
+
+        inicializarComponentes();
+
+        //Configuração Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Agendamentos Solicitados");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void inicializarComponentes() {
+        recyclerPedidos = findViewById(R.id.recyclerPedidos);
     }
 }
